ab535a8ba7bc28051ec160c166b0699e741402f1
saraaissaoui <sara.aissaoui@edu.esiee.fr>
2024-09-23 18:01:43 +0000

    exo les tuples

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
-#### Imports et définition des variables globales
-
-
-#### Fonctions secondaires
-
+"""
+Ce module contient des fonctions pour compresser des chaînes de caractères et leurs tests unitaires.
+"""
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
-
+    Compresse une chaîne de caractères en groupant les caractères identiques successifs.
+    Retourne une liste de tuples où chaque tuple contient un caractère et son nombre 
+    d'occurrences consécutives.
+    """
+    char_list = [s[0]]
+    count_list = [1]
+    n = len(s)
+    for k in range(1, n):
+        if s[k] == s[k-1]:
+            count_list[-1] += 1
+        else:
+            char_list.append(s[k])
+            count_list.append(1)
+    return list(zip(char_list, count_list))
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    Fonction récursive pour compresser une chaîne de caractères.
+    Retourne une liste de tuples où chaque tuple contient un caractère 
+    et son nombre d'occurrences consécutives.
+    """
+    def rec2(s, char_list, count_list, k):
+        if k == len(s):
+            return list(zip(char_list, count_list))
+        if s[k] == char_list[-1]:
+            count_list[-1] += 1
+        else:
+            char_list.append(s[k])
+            count_list.append(1)
+        return rec2(s, char_list, count_list, k+1)
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    return rec2(s, [s[0]], [1], 1)
 
-    return []
-    
+# Tests
+def test_artcode_i():
+    """Test de la fonction artcode_i avec une chaîne de caractères spécifique."""
+    assert artcode_i('MMMMaaacXolloMM') == [('M', 4), ('a', 3), ('c', 1), ('X', 1), ('o', 1),
+                                            ('l', 2), ('o', 1), ('M', 2)]
 
-#### Fonction principale
-
+def test_artcode_r():
+    """Test de la fonction artcode_r avec une chaîne de caractères spécifique."""
+    assert artcode_r('MMMMaaacXolloMM') == [('M', 4), ('a', 3), ('c', 1), ('X', 1), ('o', 1),
+                                            ('l', 2), ('o', 1), ('M', 2)]
 
 def main():
+    """Affiche les résultats de l'exécution des fonctions artcode_i et artcode_r."""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
